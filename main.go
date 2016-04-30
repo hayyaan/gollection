67879bba3603eb473f588631703573d82c54f092
Matthias Loibl <mail@matthiasloibl.com>
2016-04-30 02:11:11 +0200

    Use codegansta's cli not cobra
    We don't use cobra because of its init() and global commands

--- main.go
+++ main.go
@@ -1,26 +1,30 @@
 package gollection
 
 import (
-	"fmt"
+	"os"
+
+	"github.com/codegangsta/cli"
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/cobra"
 )
 
 type Gollection struct {
-	Config  GollectionConfig
-	Router  *gin.Engine
-	Command cobra.Command
+	Cli    *cli.App
+	Config GollectionConfig
+	Router *gin.Engine
 }
 
 func New() *Gollection {
-	return &Gollection{}
+	gollection := Gollection{
+		Cli: cli.NewApp(),
+	}
+
+	return &gollection
+}
+
+func (gollection *Gollection) Run() error {
+	return gollection.Cli.Run(os.Args)
 }
 
 func (gollection *Gollection) SetConfig(gc GollectionConfig) {
 	gollection.Config = gc
-}
-
-func (gollection *Gollection) Run() error {
-	addr, port := gollection.Config.GetAddrPort()
-	return gollection.Router.Run(fmt.Sprintf("%s:%d", addr, port))
 }
