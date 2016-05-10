2382617794a8711c1d26d560806dfaa39b38f3ea
Matthias Loibl <mail@matthiasloibl.com>
2016-05-10 12:18:11 +0200

    gollection.AddDB takes a gorm DB

--- gollection.go
+++ gollection.go
@@ -1,39 +1,54 @@
 package gollection
 
 import (
+	"log"
 	"os"
 
 	"github.com/codegangsta/cli"
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 )
 
 // Gollection holds everything for your application to work
 type Gollection struct {
 	Cli    *cli.App
 	Config Config
+	DB     *gorm.DB
 	Router *gin.Engine
 }
 
 // New creates a new gollection with minimum requirements
-func New(config Config) *Gollection {
+func New(c Config) *Gollection {
 	gin.SetMode(gin.ReleaseMode)
 
-	gollection := Gollection{
+	g := Gollection{
 		Cli:    cli.NewApp(),
-		Config: config,
+		Config: c,
 		Router: gin.Default(),
 	}
 
-	gollection.startCli()
+	g.startCli()
 
-	return &gollection
+	return &g
 }
 
 // Run runs your gollection application. Fingers crossed
-func (gollection *Gollection) Run() error {
-	return gollection.Cli.Run(os.Args)
+func (g *Gollection) Run() error {
+	return g.Cli.Run(os.Args)
 }
 
-func (gollection *Gollection) AddRoutes(routes func(*gin.Engine)) {
-	routes(gollection.Router)
+func (g *Gollection) AddRoutes(routes func(*gin.Engine)) {
+	routes(g.Router)
+}
+
+func (g *Gollection) AddDB(db *gorm.DB, err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if g.Config.AppConfig.Env == "local" {
+		db.LogMode(true)
+	}
+
+	g.DB = db
 }
