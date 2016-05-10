7625ecaa409bfbacbfd42bd53aa78c856c97c84c
Matthias Loibl <mail@matthiasloibl.com>
2016-05-10 12:13:51 +0200

    Create DBConfig and add it to Config

--- config.go
+++ config.go
@@ -3,11 +3,21 @@ package gollection
 type (
 	Config struct {
 		AppConfig
+		DBConfig
 	}
 	AppConfig struct {
 		Name  string
 		Usage string
 		Host  string
 		Port  int
+		Env   string
+	}
+	DBConfig struct {
+		Dialect  string
+		Host     string
+		Port     int
+		Database string
+		Username string
+		Password string
 	}
 )
