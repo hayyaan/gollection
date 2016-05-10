f7edb311ba2ce35e0e54521c27181b53527af835
Matthias Loibl <mail@matthiasloibl.com>
2016-05-10 12:17:30 +0200

    Create Postgres gorm creator

---
+++ database/postgres.go
@@ -0,0 +1,20 @@
+package database
+
+import (
+	"fmt"
+	"github.com/MetalMatze/gollection"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+func Postgres(c gollection.Config) (*gorm.DB, error) {
+	url := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable",
+		c.DBConfig.Username,
+		c.DBConfig.Password,
+		c.DBConfig.Host,
+		c.DBConfig.Port,
+		c.DBConfig.Database,
+	)
+
+	return gorm.Open("postgres", url)
+}
