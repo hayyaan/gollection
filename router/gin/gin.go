fdb6c1926460ce0b3789a3bf2a6ce7c970814550
Matthias Loibl <mail@matthiasloibl.com>
2016-06-26 23:59:13 +0200

    Add logger & config params to gin package

--- router/gin/gin.go
+++ router/gin/gin.go
@@ -1,20 +1,38 @@
 package gin
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/go-kit/kit/log"
 	"github.com/urfave/cli"
 )
 
+type GinConfig struct {
+	Host string
+	Port int
+}
+
 // Gin is a gin-gonic wrapper that implements gollection's interfaces
 type Gin struct {
+	config GinConfig
 	Engine *gin.Engine
 }
 
 // New creates a new Gin wrapper
-func New() *Gin {
-	return &Gin{
-		Engine: gin.Default(),
+func New(logger log.Logger, c GinConfig) *Gin {
+	gin.SetMode(gin.ReleaseMode)
+
+	g := &Gin{
+		config: c,
+		Engine: gin.New(),
 	}
+
+	g.Engine.Use(gin.Recovery())
+	g.Engine.Use(ginLogging(logger))
+
+	return g
 }
 
 // Cli implements the CliProvider interface and adds gin to the set of cli commands
@@ -23,8 +41,32 @@ func (g Gin) Cli() cli.Command {
 		Name:  "serve",
 		Usage: "Run the http server that listens on ", //+ addr,
 		Action: func(c *cli.Context) error {
-			g.Engine.Run(":1234")
+			g.Engine.Run(fmt.Sprintf("%s:%d", g.config.Host, g.config.Port))
 			return nil
 		},
+	}
+}
+
+// This is the official gin logger yet a bit changed to use go-kit's log.Logger interface
+func ginLogging(logger log.Logger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Start timer
+		start := time.Now()
+		path := c.Request.URL.Path
+
+		// Process request
+		c.Next()
+
+		clientIP := c.ClientIP()
+		method := c.Request.Method
+		statusCode := c.Writer.Status()
+
+		logger.Log(
+			"status", statusCode,
+			"duration", time.Since(start),
+			"method", method,
+			"ip", clientIP,
+			"path", path,
+		)
 	}
 }
--- router/gin/gin_test.go
+++ router/gin/gin_test.go
@@ -3,18 +3,19 @@ package gin
 import (
 	"testing"
 
+	"github.com/go-kit/kit/log"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGin_New(t *testing.T) {
-	r := New()
+	r := New(log.NewNopLogger(), GinConfig{})
 	assert.NotNil(t, r)
 	assert.NotNil(t, r.Engine)
 	//TODO: Test something real for this?!
 }
 
 func TestGin_Cli(t *testing.T) {
-	r := New()
+	r := New(log.NewNopLogger(), GinConfig{})
 	cli := r.Cli()
 	assert.Equal(t, "serve", cli.Name)
 	assert.Equal(t, "Run the http server that listens on ", cli.Usage)
